275f9fe3d01044cd0d87a62d4b79b74837440b52
giacomopiccinini <piccinini.giacomo@gmail.com>
2023-10-23 11:26:03 +0200

    [FEAT] Add walkdir

--- imagesum/src/main.rs
+++ imagesum/src/main.rs
@@ -1,7 +1,8 @@
 use std::collections::HashSet;
-use std::fs;
 use std::env;
 use image::io::Reader as ImageReader;
+use walkdir::WalkDir;
+use std::path::Path;
 
 fn main() {
     // Get the target directory from command line arguments
@@ -13,17 +14,8 @@ fn main() {
         return;
     }
 
-    // Get the target directory as the first argument
-    let directory = &args[1];
-
-    // Read the directory
-    let paths = match fs::read_dir(directory) {
-        Ok(p) => p,
-        Err(e) => {
-            println!("Failed to read directory {}: {}", directory, e);
-            return;
-        }
-    };
+    // Get the target
+    let target = Path::new(&args[1]);
 
     // Init the image counter
     let mut counter = 0;
@@ -31,34 +23,61 @@ fn main() {
     // Init the set of unique dimensions
     let mut dimensions: HashSet<(u32, u32)> = HashSet::new();
 
-    // Iterate over the files in the directory
-    for path in paths {
-        // Unwrap the path
-        let path = path.unwrap().path();
+    // Case where the target is a directory
+    if target.is_dir() {
 
-        // If the path has an extension
-        if let Some(ext) = path.extension() {
-            // If the extension is an image extension
-            if ["jpg", "jpeg", "png", "JPG", "bmp", "gif", "tiff"].contains(&ext.to_str().unwrap()) {
+        // Iterate over the files in the directory
+        for path in WalkDir::new(&target).into_iter().filter_map(|e| e.ok()){
 
-                // Increment the image counter
-                counter += 1;
+            // Get the path
+            let path = path.path();
 
-                // Try to read the dimensions of the image
-                match ImageReader::open(&path) {
-                    Ok(reader) => {
-                        if let Ok((width, height)) = reader.into_dimensions() {
-                            dimensions.insert((height, width));
+            if path.is_dir() {
+                continue;
+            }
+
+            // If the path has an extension
+            if let Some(ext) = path.extension() {
+                // If the extension is an image extension
+                if ["jpg", "jpeg", "png", "JPG", "bmp", "gif", "tiff"].contains(&ext.to_str().unwrap()) {
+
+                    // Increment the image counter
+                    counter += 1;
+
+                    // Try to read the dimensions of the image
+                    match ImageReader::open(&path) {
+                        Ok(reader) => {
+                            if let Ok((width, height)) = reader.into_dimensions() {
+                                dimensions.insert((height, width));
+                            }
+                        },
+                        Err(e) => {
+                            println!("Failed to read dimensions of {:?}: {}", path, e);
                         }
-                    },
-                    Err(e) => {
-                        println!("Failed to read dimensions of {:?}: {}", path, e);
                     }
                 }
             }
         }
     }
+    else {
+        if ["jpg", "jpeg", "png", "JPG", "bmp", "gif", "tiff"].contains(&target.extension().expect("Error with extension").to_str().unwrap()) {
 
+            // Increment the image counter
+            counter += 1;
+
+            // Try to read the dimensions of the image
+            match ImageReader::open(&target) {
+                Ok(reader) => {
+                    if let Ok((width, height)) = reader.into_dimensions() {
+                        dimensions.insert((height, width));
+                    }
+                },
+                Err(e) => {
+                    println!("Failed to read dimensions of {:?}: {}", target, e);
+                }
+            }
+        }
+    }
     // Print the results
     println!("Total images: {}", counter);
     println!("Unique (height, width) pairs: {:?}", dimensions);
